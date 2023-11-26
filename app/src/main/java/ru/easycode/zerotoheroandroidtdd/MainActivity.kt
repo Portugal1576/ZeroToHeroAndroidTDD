7eb52f9272dfabdf12cc3cce604c2d2f14efc568
DESKTOP-21BKGQF\Andrii <a0975550085@gmail.com>
2023-11-26 17:46:02 +0000

    task010 solved button.isEnabled

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,48 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private lateinit var ll_text: LinearLayout
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.removeButton)
+        textView = findViewById(R.id.titleTextView)
+        ll_text = findViewById(R.id.rootLayout)
+
+        button.setOnClickListener {
+            ll_text.removeView(textView)
+            button.isEnabled = false
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val status = ll_text.childCount
+        outState.putInt(KEY, status)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val status = savedInstanceState.getInt(KEY)
+        if (status == 1) ll_text.removeView(textView)
+        button.isEnabled = false
+    }
+
+    companion object{
+        private val KEY = "key"
     }
 }
