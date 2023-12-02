94ffef9707914b1a9bb6128a7c273b9ae766469c
DESKTOP-21BKGQF\Andrii <a0975550085@gmail.com>
2023-12-02 16:46:53 +0000

    task011 solved android:freezesText="true"

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,28 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var countTextView: TextView
+    private lateinit var button: Button
+    private val counts = Count.Base(step = 2)
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        countTextView = findViewById(R.id.countTextView)
+        button = findViewById(R.id.incrementButton)
+
+        button.setOnClickListener {
+            val result = counts.increment(countTextView.text.toString())
+            countTextView.text = result
+        }
     }
 }
