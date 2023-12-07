5f464898ceb72d384e045e94c9c1a728f072cb6b
DESKTOP-21BKGQF\Andrii <a0975550085@gmail.com>
2023-12-07 10:57:14 +0000

    task014 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,41 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.actionButton)
+        progressBar.visibility = View.INVISIBLE
+        textView.visibility = View.INVISIBLE
+
+
+        button.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            button.isEnabled = false
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(3500)
+                progressBar.visibility = View.INVISIBLE
+                button.isEnabled = true
+                textView.visibility = View.VISIBLE
+            }
+        }
     }
 }
